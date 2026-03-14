1f4802a126587acf4ecda5210b5a9939e751ae46
Zizidobastidor <dea.op1@gmail.com>
2026-03-14 17:53:52 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,35 +1,38 @@
-import HeroSection from "@/components/HeroSection";
-import WhatIsSection from "@/components/WhatIsSection";
-import ForWhoSection from "@/components/ForWhoSection";
-import WhyEmbroiderSection from "@/components/WhyEmbroiderSection";
-import WhatYouGetSection from "@/components/WhatYouGetSection";
-import FascinationsSection from "@/components/FascinationsSection";
-import PricingSection from "@/components/PricingSection";
-import ObjectionsSection from "@/components/ObjectionsSection";
-import AuthorSection from "@/components/AuthorSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
-import ClosingSection from "@/components/ClosingSection";
-import GuaranteeSection from "@/components/GuaranteeSection";
-import FooterSection from "@/components/FooterSection";
+  ---
+  import HeroSection from "@/components/HeroSection";
+  import WhatIsSection from "@/components/WhatIsSection";
+  import ForWhoSection from "@/components/ForWhoSection";
+  import WhyEmbroiderSection from "@/components/WhyEmbroiderSection";
+  import WhatYouGetSection from "@/components/WhatYouGetSection";
+  import FascinationsSection from "@/components/FascinationsSection";
+  import TestimonialsSection from "@/components/TestimonialsSection";
+  import AuthorSection from "@/components/AuthorSection";
+  import ObjectionsSection from "@/components/ObjectionsSection";
+  import PricingSection from "@/components/PricingSection";
+  import GuaranteeSection from "@/components/GuaranteeSection";
+  import ClosingSection from "@/components/ClosingSection";
+  import FooterSection from "@/components/FooterSection";
 
-const Index = () => {
-  return (
-    <main>
-      <HeroSection />
-      <WhatIsSection />
-      <ForWhoSection />
-      <WhyEmbroiderSection />
-      <WhatYouGetSection />
-      <FascinationsSection />
-      <PricingSection />
-      <ObjectionsSection />
-      <AuthorSection />
-      <TestimonialsSection />
-      <ClosingSection />
-      <GuaranteeSection />
-      <FooterSection />
-    </main>
-  );
-};
+  const Index = () => {
+    return (
+      <main>
+        <HeroSection />
+        <WhatIsSection />
+        <ForWhoSection />
+        <WhyEmbroiderSection />
+        <WhatYouGetSection />
+        <FascinationsSection />
+        <TestimonialsSection />
+        <AuthorSection />
+        <ObjectionsSection />
+        <PricingSection />
+        <GuaranteeSection />
+        <ClosingSection />
+        <FooterSection />
+      </main>
+    );
+  };
 
-export default Index;
+  export default Index;
+
+  ---
